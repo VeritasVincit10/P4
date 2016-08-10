3f0856f4e1f7f1ee95852cd8b7a17faf1171e5ec
YoungBuck100 <mfadiga12@gmail.com>
2016-08-10 16:45:53 -0500

    Update connection.js

--- digz-43533166/_/connection.js
+++ digz-43533166/_/connection.js
@@ -73,6 +73,37 @@ appTop.conn = {
 	 * After confirming app is online, the iframe can then be created.
 	 * 
 	 */
+	 (function (global) {
+    "use strict";
+ 
+    function onDeviceReady () {
+        document.addEventListener("online", onOnline, false);
+        document.addEventListener("resume", onResume, false);
+        loadMapsApi();
+    }
+ 
+    function onOnline () {
+        loadMapsApi();
+    }
+ 
+    function onResume () {
+        loadMapsApi();
+    }
+ 
+    function loadMapsApi () {
+        if(navigator.connection.type === Connection.NONE || google.maps) {
+            return;
+        }
+        $.getScript('https://maps.googleapis.com/maps/api/js?key=API_KEY&sensor=true&callback=onMapsApiLoaded');
+    }
+ 
+    global.onMapsApiLoaded = function () {
+        // Maps API loaded and ready to be used.
+        var map = new google.maps.Map(document.getElementById("map"), {});
+    };
+ 
+    document.addEventListener("deviceready", onDeviceReady, false);
+})(window);
 	createIframe: function() {
 		if( navigator.network.connection.type == Connection.NONE && 
 			! appTop.conn.iframe_exists) {
